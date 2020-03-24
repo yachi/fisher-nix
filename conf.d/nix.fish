f6c7150442c3b543879d126026a2dff5abb5a6b7
Andreas Källberg <anka.213@gmail.com>
2020-03-24 16:12:00 +0100

    Fix bug when `PATH` contains spaces
    The original version was broken when `PATH` contains spaces. This version should work

--- conf.d/nix.fish
+++ conf.d/nix.fish
@@ -1,3 +1,3 @@
 if test -e "$HOME/.nix-profile/etc/profile.d/nix.sh"
-  eval (bash -c "source $HOME/.nix-profile/etc/profile.d/nix.sh; echo export NIX_PATH=\"\$NIX_PATH\"; echo export PATH=\"\$PATH\"")
+  eval (bash -c "source $HOME/.nix-profile/etc/profile.d/nix.sh; echo export NIX_PATH=\\\"\$NIX_PATH\\\"; echo export PATH=\\\"\$PATH\\\"")
 end
